212b2fe68c47435d88f45e1d152cd5a291c52e02
Saif-Rahman-801 <saifurs102@gmail.com>
2023-11-14 19:25:36 +0600

    used custom hook to load data

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from "react";
 import MenuItem from "../../Shared/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => setMenu(data.filter((d) => d.category === "popular")));
-  }, []);
+  const [menu, loading] = useMenu();
+  if (loading) {
+    return <h2>Loading..... </h2>;
+  }
+  const popular = menu.filter((d) => d.category === "popular");
 
   return (
     <section className="my-5">
@@ -16,7 +14,7 @@ const PopularMenu = () => {
         <h2 className="text-center text-3xl fotn-bold">Form our menu</h2>
       </header>
       <div className="grid md:grid-cols-2 gap-4 my-10">
-        {menu.map((item) => (
+        {popular.map((item) => (
           <MenuItem key={item._id} item={item} />
         ))}
       </div>
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,15 +1,19 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+// import menuImg from "../../../assets/menu/menu-bg.png";
+import menuImg from "../../../assets/menu/banner3.jpg"
+
 
 const Menu = () => {
-    return (
-        <div>
-            <Helmet>
-                <title>
-                    Bistro Boss | menu
-                </title>
-            </Helmet>
-        </div>
-    );
+  return (
+    
+    <div>
+      <Helmet>
+        <title>Bistro Boss | menu</title>
+      </Helmet>
+      <Cover img={menuImg} title="our menu" />
+    </div>
+  );
 };
 
 export default Menu;
