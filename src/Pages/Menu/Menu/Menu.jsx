13613a87acc0eaff4cf1f1319ec929228db293b7
Saif-Rahman-801 <saifurs102@gmail.com>
2023-11-14 19:51:43 +0600

    our menu loaded

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,17 +1,33 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 // import menuImg from "../../../assets/menu/menu-bg.png";
-import menuImg from "../../../assets/menu/banner3.jpg"
-
+import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import useMenu from "../../../hooks/useMenu";
+import MenuCategory from "./MenuCategory/MenuCategory";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg"
+import soupImg from "../../../assets/menu/soup-bg.jpg"
+import saladImg from "../../../assets/menu/salad-bg.jpg"
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const dessert = menu.filter((d) => d.category === "dessert");
+  const soup = menu.filter((d) => d.category === "soup");
+  const pizza = menu.filter((d) => d.category === "pizza");
+  const salad = menu.filter((d) => d.category === "salad");
+  const offered = menu.filter((d) => d.category === "offered");
   return (
-    
     <div>
       <Helmet>
         <title>Bistro Boss | menu</title>
       </Helmet>
       <Cover img={menuImg} title="our menu" />
+      <MenuCategory items={offered} />
+      <MenuCategory items={dessert} title={`dessert`} coverImg={dessertImg} />
+      <MenuCategory items={pizza} title={`pizza`} coverImg={pizzaImg} />
+      <MenuCategory items={soup} title={`soup`} coverImg={soupImg} />
+      <MenuCategory items={salad} title={`salad`} coverImg={saladImg} />
+      {/* <MenuCategory items={offered} /> */}git 
     </div>
   );
 };
--- src/Pages/Menu/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/Menu/MenuCategory/MenuCategory.jsx
@@ -1,10 +1,26 @@
+import PropTypes from "prop-types";
+import MenuItem from "../../../Shared/MenuItem";
+import Cover from "../../../Shared/Cover/Cover";
 
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({ items, title, coverImg }) => {
+  return (
+    <div>
+      {title && <Cover img={coverImg} title={title} />}
+      <div className="grid md:grid-cols-2 gap-4 my-10">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+MenuCategory.propTypes = {
+  coverImg: PropTypes.any,
+  items: PropTypes.shape({
+    map: PropTypes.func,
+  }),
+  title: PropTypes.any,
 };
 
 export default MenuCategory;
