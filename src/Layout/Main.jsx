c69dc7e831485fe26ed4ff3a210a86b43121a2af
Saif-Rahman-801 <saifurs102@gmail.com>
2023-11-18 12:44:35 +0600

    login form created

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const Main = () => {
-    return (
-        <div>
-            <Navbar />
-            <Outlet />
-            <Footer />
-        </div>
-    );
+  const location = useLocation();
+  const noHeaderFooter = location.pathname.includes("login");
+  return (
+    <div>
+      {noHeaderFooter || <Navbar />}
+      <Outlet />
+      {noHeaderFooter || <Footer />}
+    </div>
+  );
 };
 
 export default Main;
--- src/Pages/Order/Order/OrderTab.jsx
+++ src/Pages/Order/Order/OrderTab.jsx
@@ -1,20 +1,45 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 import FoodCard from "../../Shared/FoodCard/FoodCard";
+/* import { Swiper, SwiperSlide } from 'swiper/react';
 
-const OrderTab = ({items}) => {
-    return (
-        <div className="grid grid-cols-3 space-y-5">
-            {items.map((item) => (
-              <FoodCard key={item._id} item={item} />
-            ))}
-          </div>
-    );
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules'; */
+
+const OrderTab = ({ items }) => {
+  /* const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  }; */
+  return (
+    <div className="grid grid-cols-3 space-y-5">
+      {items.map((item) => (
+        <FoodCard key={item._id} item={item} />
+      ))}
+
+      {/* <Swiper
+            pagination={pagination}
+            modules={[pagination]}
+            className="mySwiper"
+            >
+              <SwiperSlide>
+
+              </SwiperSlide>
+            </Swiper> */}
+    </div>
+  );
 };
 
 OrderTab.propTypes = {
   items: PropTypes.shape({
-    map: PropTypes.func
-  })
-}
+    map: PropTypes.func,
+  }),
+};
 
 export default OrderTab;
