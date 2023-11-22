ae308036012ddd759d83d9314ddd3c5fa3e41cb1
Saif-Rahman-801 <saifurs102@gmail.com>
2023-11-22 18:58:10 +0600

    used axios interceptor to send access token

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,31 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
-    return axiosSecure
+  axiosSecure.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("access-token");
+      console.log("requ intercepted", token);
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    const status = error.response.status
+    console.log("error status in the interceptor", status);
+    return Promise.reject(error);
+  });
+
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
