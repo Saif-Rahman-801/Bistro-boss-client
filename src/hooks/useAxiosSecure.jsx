c4348e4ce0e6661f481bf02611da22982597b6fc
Saif-Rahman-801 <saifurs102@gmail.com>
2023-11-22 19:37:23 +0600

    checking admin role

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,14 +1,19 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
   axiosSecure.interceptors.request.use(
     (config) => {
       const token = localStorage.getItem("access-token");
-      console.log("requ intercepted", token);
+      // console.log("requ intercepted", token);
       config.headers.authorization = `Bearer ${token}`;
       return config;
     },
@@ -17,17 +22,20 @@ const useAxiosSecure = () => {
     }
   );
 
-  axios.interceptors.response.use(function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  }, function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    const status = error.response.status
-    console.log("error status in the interceptor", status);
-    return Promise.reject(error);
-  });
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      console.log("error status in the interceptor", error);
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
 
   return axiosSecure;
 };
